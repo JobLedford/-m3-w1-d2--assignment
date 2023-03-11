7ea312157062ddf6a25fbb607ab13e1917b57755
JobLedford <j.ledford.421@westcliff.edu>
2023-03-11 16:32:24 -0500

    hopoe it works

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
-var express = require("express");
-var router = express.Router();
-var { check, validationResult } = require("express-validator");
+const express = require("express");
+const mongoose = require('mongoose');
+const { check, validationResult } = require("express-validator");
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+
+const path = require('path');
+const auth = require('http-auth');
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
 
 router.get("/", function (req, res) {
   res.render("form", { title: "Registration form" });
@@ -14,9 +23,15 @@ router.post(
   ],
   function (req, res) {
     //console.log(req.body);
-    var errors = validationResult(req);
+    const errors = validationResult(req);
     if (errors.isEmpty()) {
-      res.send("Thank you for you registration!");
+      const registration = new Registration(req.body);
+      registration.save()
+      .then(() => {res.send("Thank you for you registration!"); })
+      .catch((err) => {
+        console.log(err);
+        res.send('Sorry! Something went wrong.');
+      });
     } else {
       res.render("form", {
         title: "Registration form",
@@ -26,5 +41,15 @@ router.post(
     }
   }
 );
+
+router.get('/registrations', basic.check((req,res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render("index", { title: "Listing registrations", registrations });
+})
+  .catch(() => {
+    res.send("Sorry! Something went wrong.");
+  })
+}));
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,20 +1,23 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+/*mongoose.connect('mongodb://localhost:27017/formapp')*/
 
 mongoose.connection
-    .on('open', () => {
-        console.log('Mongoose connection open');
-    })
-    .on('error', (err) => {
-        console.log(`Connection error: ${err.message}`);
-    });
+  .on("open", () => {
+    console.log("Mongoose connection open");
+  })
+  .on("error", (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
 
+
+require('./model/Registration');
 const app = require("./app");
-const server = app.listen(3000, function() {
-    console.log(`Express is runnin on port ${server.address().port}`);
-})
+const server = app.listen(3000, function () {
+  console.log(`Express is runnin on port ${server.address().port}`);
+});
