cd966f2d8a2acfa6f258fc132161b8cc4c86d73d
JobLedford <j.ledford.421@westcliff.edu>
2023-03-11 12:49:23 -0500

    mongoose problem

--- start.js
+++ start.js
@@ -1,5 +1,20 @@
-var app = require('./app');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-var server = app.listen(3000, function() {
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection
+    .on('open', () => {
+        console.log('Mongoose connection open');
+    })
+    .on('error', (err) => {
+        console.log(`Connection error: ${err.message}`);
+    });
+
+const app = require("./app");
+const server = app.listen(3000, function() {
     console.log(`Express is runnin on port ${server.address().port}`);
 })
